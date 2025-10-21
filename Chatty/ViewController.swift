3a4918dd94eae891742b6695d486324451486b40
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2025-10-21 16:22:34 +0300

    Initial Commit

---
+++ Chatty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatty
+//
+//  Created by Alexandra on 21.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
